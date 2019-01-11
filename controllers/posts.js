f9f4d8041c8118602aafb8cff9c02d6ee616dcbc
Richard Mena <richardmf9425@yahoo.com>
2019-01-11 12:49:58 -0500

    Add cod for Post Create

---
+++ controllers/posts.js
@@ -0,0 +1,21 @@
+const Post = require('../models/post');
+
+module.exports = {
+	// Posts Index
+	async getPosts(req, res, next) {
+		let posts = Post.find({});
+		res.render('posts/index', {
+			posts
+		});
+	},
+	// Posts New
+	newPost(req, res, next) {
+		res.render('posts/new');
+	},
+	// Posts Create
+	async createPost(req, res, next) {
+		//use req.body to create a new Post
+		let post = await Post.create(req.body);
+		res.redirect(`/posts/${post.id}`);
+	}
+};
--- routes/posts.js
+++ routes/posts.js
@@ -1,43 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const {
+	errorHandler
+} = require('../middleware');
+const {
+	getPosts,
+	newPost,
+	createPost
+} = require('../controllers/posts');
 
 /* GET posts index /posts */
-router.get('/', (req, res, next) => {
-    res.send(' INDEX /posts');
-  });
+router.get('/', errorHandler(getPosts));
 
-  /* GET posts new /posts/new */
-router.get('/new', (req, res, next) => {
-    res.send(' NEW /posts/new');
-  });
+/* GET posts new /posts/new */
+router.get('/new', newPost);
 
+/* POST posts create /posts */
+router.post('/', errorHandler(createPost));
 
-  /* POST posts create /posts */
-router.post('/', (req, res, next) => {
-    res.send('CREATE /posts');
-  });
-
-
-  /* GET posts show /posts/:id */
+/* GET posts show /posts/:id */
 router.get('/:id', (req, res, next) => {
-    res.send(' SHOW /posts/:id');
-  });
+	res.send(' SHOW /posts/:id');
+});
 
-
-  /* GET posts edit /posts/:id/edit */
+/* GET posts edit /posts/:id/edit */
 router.get('/:id/edit', (req, res, next) => {
-    res.send(' EDIT /posts/:id/edit');
-  });
+	res.send(' EDIT /posts/:id/edit');
+});
 
-  /* PUT posts update /posts/:id */
+/* PUT posts update /posts/:id */
 router.put('/:id', (req, res, next) => {
-    res.send(' UPDATE /posts/:id');
-  });
+	res.send(' UPDATE /posts/:id');
+});
 
-  /* DELETE posts destroy /posts/:id */
+/* DELETE posts destroy /posts/:id */
 router.delete('/:id', (req, res, next) => {
-    res.send(' DELETE /posts/:id');
-  });
-  
-  module.exports = router;
-  
+	res.send(' DELETE /posts/:id');
+});
+
+module.exports = router;
