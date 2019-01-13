00485eab1a525ff47a322b5e571f7404259d2207
Richard Mena <richardmf9425@yahoo.com>
2019-01-13 13:28:49 -0500

    Add client side validation for add and remove pics

--- models/user.js
+++ models/user.js
@@ -3,15 +3,8 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-email: String,
-image: String,
-posts: [
-    {
-        type: Schema.Types.ObjectId,
-        ref: 'Post'
-
-    }
-  ]
+    email: String,
+    image: String
 });
 
 UserSchema.plugin(passportLocalMongoose);
