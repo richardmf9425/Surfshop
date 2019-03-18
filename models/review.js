d8020802c1fe33f7a15f55e42b345cf104c32b45
Richard Mena <richardmf9425@yahoo.com>
2019-03-18 19:23:50 -0400

    push to github

--- models/review.js
+++ models/review.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ReviewSchema = new Schema({
-Review: String,
-author: {
-    type: Schema.Types.ObjectId,
-    ref: 'User'
+    Review: String,
+    rating: Number,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
     }
 });
 
